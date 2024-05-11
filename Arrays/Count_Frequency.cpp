cb7a743f0966ee4f2af42444c2a5a4a4916fc412
SAwanish <awanuj2203@gmail.com>
2024-05-11 16:07:28 +0530

    add frequency count

---
+++ Arrays/Count_Frequency.cpp
@@ -0,0 +1,9 @@
+#include<vector.h>
+vector<int> countFrequency(int n, int x, vector<int> &nums){
+    // Write your code here.
+    vector<int>hash(n,0);
+    for(int i=0;i<n;i++){
+        hash[nums.at(i)-1]+=1;
+    }
+    return hash;
+}
