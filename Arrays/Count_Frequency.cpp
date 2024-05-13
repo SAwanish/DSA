45efc1eaf1a2c08b792375186472421230c454df
SAwanish <awanuj2203@gmail.com>
2024-05-13 23:21:01 +0530

    code upgradation

--- Arrays/Count_Frequency.cpp
+++ Arrays/Count_Frequency.cpp
@@ -1,7 +1,8 @@
-#include<vector.h>
+#include<vector>
+using  namespace std;
 vector<int> countFrequency(int n, int x, vector<int> &nums){
     // Write your code here.
-    vector<int>hash(n,0);
+    vector<int> hash(n,0);
     for(int i=0;i<n;i++){
         hash[nums.at(i)-1]+=1;
     }
--- Arrays/reverse.cpp
+++ Arrays/reverse.cpp
@@ -1,11 +1,12 @@
-#include<vector.h>
-
+#include<vector>
+#include<iostream>
+using namespace std;
 vector<int> reverseArray(int n, vector<int> &nums)
 {
     // Write your code here.
     if (n>0){
         if(n==nums.size()) nums.pop_back();
-        cout<<*(nums.end())<<" ";
+        std::cout<<*(nums.end())<<" ";
         nums.pop_back();
         reverseArray(n-1, nums);
     }
